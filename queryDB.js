156d3cf1960d308586d3ec7ea3b8baa22e7a6f6e
Rodion Sibov <rodionsibov@gmail.com>
2023-02-07 22:49:27 +0200

    Update

--- queryDB.js
+++ queryDB.js
@@ -1,10 +1,30 @@
 import fs from "fs";
 import { exit } from "process";
 
-fs.readFile("db.json", function (error, data) {
-  if (error) {
+export default async function queryDB(externalFunction) {
+  try {
+    let info = [];
+    if (fs.existsSync("db.json")) {
+      await fs.readFile("db.json", function (error, data) {
+        if (error) {
+          console.log("Reading File Failed!", error);
+          return;
+        }
+
+        info = JSON.parse(data.toString());
+
+        if (externalFunction && !error) {
+          externalFunction(info);
+          return;
+        }
+      });
+    } else {
+      if (externalFunction) {
+        externalFunction(info);
+        return;
+      }
+    }
+  } catch (error) {
     console.log("Something went wrong", error);
-    exit(1);
   }
-  console.log(data);
-});
+}
